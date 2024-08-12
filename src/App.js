b4d509cff14aa8450c79b6d1ea873e434b81a1cb
Rugved <guravrugved@gmail.com>
2024-08-12 13:45:11 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <h1>test</h1>
+      </main>
     </div>
   );
 }
